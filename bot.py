50d7ac38d9b253b735834800abacb268fe4addef
Dl <121394073+ZaWaRuDoToKiYoToMaRe@users.noreply.github.com>
2024-01-13 15:37:56 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,37 +1,31 @@
-from config import token
-#!/usr/bin/python
+import telebot # библиотека telebot
+from config import token # импорт токена
 
-# This is a simple echo bot using the decorator mechanism.
-# It echoes any incoming text messages.
+bot = telebot.TeleBot(token) 
 
-import telebot
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(commands=['ban'])
+def ban_user(message):
+    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
+        chat_id = message.chat.id # сохранение id чата
+         # сохранение id и статуса пользователя, отправившего сообщение
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+         # проверка пользователя
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно забанить администратора.")
+        else:
+            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.chat_join_request_handler()
+def make_some(message: telebot.types.ChatJoinRequest):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
-bot = telebot.TeleBot(token)
-
-
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, "Hi")
-    bot.send_message(message.chat.id, "Hello")
-
-@bot.message_handler(commands=['random'])
-def random_handler(message):
-    n = random.randint(1,100)
-    bot.reply_to(message, f'случайное число - {n}')
-
-@bot.message_handler(commands=['quote'])
-def quote_message(message):
-    bot.reply_to(message, "Мудрая цитата")
-    bot.send_message(message.chat.id, 'Зажиточно жить — надо фарм любить')
-
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    string = 'Ты сказал: ' + message.text
-    bot.reply_to(message, string)
-
-
-bot.infinity_polling()
+bot.infinity_polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+token = "6373768309:AAHMxHUEuyFwZxT1m7kpWkCcddrwmNMP7WQ" # токен для бота
